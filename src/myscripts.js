3a158e23406df1443f9d4b92b98ef76ef3c15164
Christian Wittorp <christian@swarminteractive.com>
2023-08-03 10:56:43 -0400

    setup image album and finish mobile responsive design

--- src/myscripts.js
+++ src/myscripts.js
@@ -1,3 +1,5 @@
+const arrayOfLeft = ["3.4rem", "-14.2em", "-32rem", "-49.7rem", "-67.5rem"];
+
 function buttonLeft() {
   let element = parseInt(
     getComputedStyle(document.documentElement).getPropertyValue("--view")
@@ -6,7 +8,14 @@ function buttonLeft() {
   if (element <= 0) {
     return;
   } else {
+    console.log(element);
     document.documentElement.style.setProperty("--view", element - 1);
+    console.log(element);
+
+    document.documentElement.style.setProperty(
+      "--left",
+      arrayOfLeft[element - 1]
+    );
   }
 }
 function buttonRight() {
@@ -18,5 +27,12 @@ function buttonRight() {
     return;
   } else {
     document.documentElement.style.setProperty("--view", element + 1);
+
+    console.log(element);
+
+    document.documentElement.style.setProperty(
+      "--left",
+      arrayOfLeft[element + 1]
+    );
   }
 }
