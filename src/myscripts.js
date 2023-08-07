ffe947df840cfafe8a567a73264a5f4dbbb0d449
Christian Wittorp <christian@swarminteractive.com>
2023-08-07 10:47:52 -0400

    tablet slides section

--- src/myscripts.js
+++ src/myscripts.js
@@ -1,4 +1,20 @@
 const arrayOfLeft = ["3.4rem", "-14.2em", "-32rem", "-49.7rem", "-67.5rem"];
+const arrayOfLeftTablet = [
+  "6.67rem",
+  "-27.86em",
+  "-62.8rem",
+  "-97.53em",
+  "-132.47rem",
+];
+const arrayOfLeftDesktop = [
+  "3.4rem",
+  "-14.2em",
+  "-32rem",
+  "-49.7rem",
+  "-67.5rem",
+];
+const arrayValues = [3.4, -14.2, -32, -49.7, -67.5];
+const tabletValue = 1.9625;
 
 function buttonLeft() {
   let element = parseInt(
@@ -8,14 +24,24 @@ function buttonLeft() {
   if (element <= 0) {
     return;
   } else {
-    console.log(element);
-    document.documentElement.style.setProperty("--view", element - 1);
-    console.log(element);
+    if (window.innerWidth < 768) {
+      console.log(element);
+      document.documentElement.style.setProperty("--view", element - 1);
+      console.log(element);
 
-    document.documentElement.style.setProperty(
-      "--left",
-      arrayOfLeft[element - 1]
-    );
+      document.documentElement.style.setProperty(
+        "--left",
+        arrayValues[element - 1] + "em"
+      );
+    } else if (window.innerHeight < 1440) {
+      document.documentElement.style.setProperty("--view", element - 1);
+
+      document.documentElement.style.setProperty(
+        "--leftTablet",
+        arrayValues[element - 1] * tabletValue + "em"
+      );
+    } else {
+    }
   }
 }
 function buttonRight() {
@@ -26,13 +52,23 @@ function buttonRight() {
   if (element >= 4) {
     return;
   } else {
-    document.documentElement.style.setProperty("--view", element + 1);
+    if (window.innerWidth < 768) {
+      document.documentElement.style.setProperty("--view", element + 1);
 
-    console.log(element);
+      console.log(element);
 
-    document.documentElement.style.setProperty(
-      "--left",
-      arrayOfLeft[element + 1]
-    );
+      document.documentElement.style.setProperty(
+        "--left",
+        arrayValues[element + 1] * tabletValue + "em"
+      );
+    } else if (window.innerHeight < 1440) {
+      document.documentElement.style.setProperty("--view", element + 1);
+
+      document.documentElement.style.setProperty(
+        "--leftTablet",
+        arrayValues[element + 1] * tabletValue + "em"
+      );
+    } else {
+    }
   }
 }
