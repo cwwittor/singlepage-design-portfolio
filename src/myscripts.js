e9bf0bc4bb9ceb83a46e8ceddc8bc356b7cda563
Christian Wittorp <christian@swarminteractive.com>
2023-08-09 10:55:49 -0400

    desktop finish

--- src/myscripts.js
+++ src/myscripts.js
@@ -6,15 +6,10 @@ const arrayOfLeftTablet = [
   "-97.53em",
   "-132.47rem",
 ];
-const arrayOfLeftDesktop = [
-  "3.4rem",
-  "-14.2em",
-  "-32rem",
-  "-49.7rem",
-  "-67.5rem",
-];
+const arrayOfLeftDesktop = ["27rem", "-8rem", "-44.5rem", "-80rem", "-115rem"];
 const arrayValues = [3.4, -14.2, -32, -49.7, -67.5];
 const tabletValue = 1.9625;
+const desktopValue = 1.65;
 
 function buttonLeft() {
   let element = parseInt(
@@ -33,7 +28,7 @@ function buttonLeft() {
         "--left",
         arrayValues[element - 1] + "em"
       );
-    } else if (window.innerHeight < 1440) {
+    } else if (window.innerWidth < 1440) {
       document.documentElement.style.setProperty("--view", element - 1);
 
       document.documentElement.style.setProperty(
@@ -41,6 +36,12 @@ function buttonLeft() {
         arrayValues[element - 1] * tabletValue + "em"
       );
     } else {
+      document.documentElement.style.setProperty("--view", element - 1);
+
+      document.documentElement.style.setProperty(
+        "--leftDesktop",
+        arrayOfLeftDesktop[element - 1]
+      );
     }
   }
 }
@@ -53,6 +54,7 @@ function buttonRight() {
     return;
   } else {
     if (window.innerWidth < 768) {
+      console.log("how");
       document.documentElement.style.setProperty("--view", element + 1);
 
       console.log(element);
@@ -61,7 +63,9 @@ function buttonRight() {
         "--left",
         arrayValues[element + 1] * tabletValue + "em"
       );
-    } else if (window.innerHeight < 1440) {
+    } else if (window.innerWidth < 1440) {
+      console.log("wow");
+
       document.documentElement.style.setProperty("--view", element + 1);
 
       document.documentElement.style.setProperty(
@@ -69,6 +73,13 @@ function buttonRight() {
         arrayValues[element + 1] * tabletValue + "em"
       );
     } else {
+      console.log("yo");
+      document.documentElement.style.setProperty("--view", element + 1);
+
+      document.documentElement.style.setProperty(
+        "--leftDesktop",
+        arrayOfLeftDesktop[element + 1]
+      );
     }
   }
 }
